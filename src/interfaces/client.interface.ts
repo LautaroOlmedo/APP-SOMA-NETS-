7c9a8c5b142edcf5d1306ea995a166079c125af9
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 <lautaroolmedo77@gmail.com>
2024-02-25 11:56:50 -0300

    updated clientInterface

--- src/interfaces/client.interface.ts
+++ src/interfaces/client.interface.ts
@@ -1,5 +1,24 @@
+import { BrandEntity } from '.././brands/entities/brand.entity';
+import { CountryEntity } from '.././countries/entities/country.entity';
+import { DepartmentEntity } from '.././departments/entities/department.entity';
+import { DirectionsEntity } from '.././directions/entities/directions.entity';
+import { EmailsEntity } from '.././emails/entities/emails.entity';
+import { PhonesEntity } from '.././phones/entities/phones.entity';
+import { ProvinceEntity } from '.././provinces/entities/province.entity';
+import { PurchaseEntity } from '.././purchases/entities/purchase.entity';
+import { StoreClientsEntity } from '.././stores/entities/store-clients.entity';
+
 export interface ClientInterface {
   firstname: string;
   lastname: string;
   dni: string;
+  emails?: EmailsEntity[];
+  phones?: PhonesEntity[];
+  purchases?: PurchaseEntity[];
+  brand?: BrandEntity;
+  direction?: DirectionsEntity;
+  storesIncludes?: StoreClientsEntity[];
+  department?: DepartmentEntity;
+  province?: ProvinceEntity;
+  country?: CountryEntity;
 }
